359c4830c4089f1a6ef2ed5c651ca6a23182bab6
Marc Chantreux <marcc@cpan.org>
2013-02-14 13:05:48 +0000

    Import of MARCC/marc-mir-template-0.1 from CPAN.
    gitpan-cpan-distribution: marc-mir-template
    gitpan-cpan-version:      0.1
    gitpan-cpan-path:         MARCC/marc-mir-template-0.1.tar.gz
    gitpan-cpan-author:       MARCC
    gitpan-cpan-maturity:     released

--- lib/MARC/MIR/Template.pm
+++ lib/MARC/MIR/Template.pm
@@ -1,6 +1,12 @@
 package MARC::MIR::Template;
 use Modern::Perl;
 use YAML ();
+sub FOR_MIR  { 0 }
+sub FOR_DATA { 1 }
+sub OPT      { 2 }
+
+our $DEBUG   = 0;
+our $VERSION = '0.1';
 
 # ABSTRACT: templating system for marc records
 
@@ -24,9 +30,11 @@ sub _data_data {
 sub _data_prepare_data {
     my ( $template, $k, $v ) = @_;
     while ( my ( $subk, $subv ) = each %$v ) {
-        $$template{data}{ $subv } = _data_data $k, $subk;
+        $$template[FOR_DATA]{ $subv } = _data_data $k, $subk;
     }
 }
+
+sub by_tag { $$a[0] cmp $$b[0] }
 
 sub _data_mvalued {
     my ( $k, $rspec ) = @_;
@@ -38,7 +46,7 @@ sub _data_mvalued {
             my $item = $_;
             # TODO: optimize by not sorting every subfield ?
             # (it's 2am, sorry) 
-            [ sort { $$a[0] cmp $$b[0] } map {  
+            [ map {  
                 my $tag = $spec{$_} or die;
                 map {
                     if ( ref ) {  map [ $tag, $_], @$_ }
@@ -50,35 +58,99 @@ sub _data_mvalued {
 }
 
 sub new {
-    my ( $pkg, $spec ) = @_;
-    my %template;
+    my ( $pkg, $spec, $options ) = @_;
+    my $template = [ $spec ];
     while ( my ( $k, $v ) = each %$spec ) {
         given ( ref $v ) {
-            when ('')     { $template{data}{ $v } = _data_control $k }
-            when ('HASH') { _data_prepare_data \%template, $k, $v }
+            when ('')     { $$template[FOR_DATA]{ $v } = _data_control $k }
+            when ('HASH') { _data_prepare_data $template, $k, $v }
             when ('ARRAY') {
                 my ( $mvalued, $fieldspec ) = @$v;
-                $template{data}{ $mvalued } = _data_mvalued $k, $fieldspec;
+                $$template[FOR_DATA]{ $mvalued } = _data_mvalued $k, $fieldspec;
             }
         }
+    };
+    $template->[OPT] = $options || {};
+    bless $template, __PACKAGE__;
+}
+
+sub debug {
+    my $self = shift;
+    for ($self->[OPT]{debug}) {
+        @_ and $_ = shift;
+        return $_;
     }
-    bless \%template, __PACKAGE__;
 }
 
 sub data {
     my ( $template, $source ) = @_;
     my $out = {};
     while ( my ( $k, $v ) = each %$source ) {
-        my $cb = $$template{data}{ $k } or next;
+        my $cb = $$template[FOR_DATA]{ $k } or next;
         $cb->( $out, $v );
     }
     [ map {
         my $field = $_;
         my $data = $$out{$field};
-        if ( ref $data ) { map { [$field, $_ ] } @$data }
+        if ( ref $data ) {
+            map {
+                # sorting keys clearly is a middleware! so the next line must 
+                # be replaced by
+                # [ $field, $_ ]
+                # also remove the t/00* 
+                [$field, [ sort by_tag @$_ ] ]
+            } @$data
+        }
         else { [ $field, $data ] }
       } sort keys %$out ]
 
+}
+
+sub _set_or_push_value {
+    my ( $target, $key, $v ) = @_;
+    for ( $$target{$key} ) {
+        if (defined) {
+            # so it happens to be multivalued
+            if (ref) { push @$_, $v  } # and i knew it :)
+            else     { $_ = [$_, $v] } # gee!
+        }
+        # the first time: just store $v
+        else { $_ = $v }
+    }
+}
+
+sub _mir_hash {
+    my ( $data, $spec, $subfields ) = @_;
+    for my $s ( @$subfields ) {
+        my ( $tag, $v ) = @$s;
+        my $key = $$spec{ $tag };
+        if ( defined $key ) { _set_or_push_value $data, $key, $v }
+        else { $DEBUG && warn "can't manage $tag" }
+    }
+}
+
+sub mir {
+    my ( $template, $fields ) = @_;
+    my $tmpl = $$template[FOR_MIR];
+    my %data;
+    for (@$fields) {
+        my ($tag,$v,$ind) = @$_;
+        my $spec = $$tmpl{ $tag } or do {
+            say STDERR "unsuported,$tag" if $template->debug;
+            next;
+        };
+        if ( my $ref = ref $spec ) {
+            if    ( $ref eq 'HASH'  ) { _mir_hash \%data, $spec, $v }
+            elsif ( $ref eq 'ARRAY' ) {
+                push @{ $data{ $$spec[0] } ||= [] }
+                , my $entry = {};
+                _mir_hash $entry, $$spec[1], $v
+            }
+            else { die "don't know how to manage $ref" }
+        }
+        else { $data{$spec} = $v }
+    }
+    \%data;
 }
 
 1;
